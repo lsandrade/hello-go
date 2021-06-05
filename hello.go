246f428713ca36e7dc440fca8b84bb28f83636ce
Luan Andrade <luansandrade92@hotmail.com>
2021-06-05 17:05:52 -0300

    Creating menu

--- hello.go
+++ hello.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	var name = "Luan"
-	var version float32 = 1.1
-	age := 29
-	fmt.Println("Hello, Mr. ", name)
-	fmt.Println("This program is in version ", version)
-	fmt.Println("Your age is ", age)
+	// var name = "Luan"
+	// var version float32 = 1.1
+	// age := 29
+	// fmt.Println("Hello, Mr. ", name)
+	// fmt.Println("This program is in version ", version)
+	// fmt.Println("Your age is ", age)
 
-	fmt.Println("Type of variable 'name' is ", reflect.TypeOf(name))
+	// fmt.Println("Type of variable 'name' is ", reflect.TypeOf(name))
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("Comando escolhido foi ", comando)
 }
