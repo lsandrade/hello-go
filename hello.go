1afdf44103cb8f948fe860c5952280a6cc253cea
Luan Andrade <luansandrade92@hotmail.com>
2021-06-05 17:21:51 -0300

    Creating functions

--- hello.go
+++ hello.go
@@ -2,35 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"reflect"
 )
 
 func main() {
-	// var name = "Luan"
-	// var version float32 = 1.1
-	// age := 29
-	// fmt.Println("Hello, Mr. ", name)
-	// fmt.Println("This program is in version ", version)
-	// fmt.Println("Your age is ", age)
 
-	// fmt.Println("Type of variable 'name' is ", reflect.TypeOf(name))
-
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do programa")
-
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("Comando escolhido foi ", comando)
-
-	// if comando == 1 {
-	// 	fmt.Println("Monitorando...")
-	// } else if comando == 2 {
-	// 	fmt.Println("Exibindo logs")
-	// } else if comando == 0 {
-	// 	fmt.Println("Saindo do programa")
-	// } else {
-	// 	fmt.Println("Não conheço esse comando")
-	// }
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -39,7 +19,34 @@ func main() {
 		fmt.Println("Exibindo logs")
 	case 0:
 		fmt.Println("Saindo do programa")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço esse comando")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+
+	var name = "Luan"
+	var version float32 = 1.1
+	age := 29
+	fmt.Println("Hello, Mr. ", name)
+	fmt.Println("This program is in version ", version)
+	fmt.Println("Your age is ", age)
+
+	fmt.Println("Type of variable 'name' is ", reflect.TypeOf(name))
+}
+
+func leComando() int {
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("Comando escolhido foi ", comando)
+	return comando
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
 }
