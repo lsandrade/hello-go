badb4c2badbdaf8e5d2f284e595f88ed1ce8594a
Luan Andrade <luansandrade92@hotmail.com>
2021-06-05 17:38:48 -0300

    Monitoring random status code

--- hello.go
+++ hello.go
@@ -10,20 +10,22 @@ import (
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
+	for {
+		exibeMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo logs")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço esse comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço esse comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -54,7 +56,11 @@ func exibeMenu() {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://www.alura.com.br"
+	site := "https://random-status-code.herokuapp.com"
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. StatusCode:", resp.StatusCode)
+	}
 }
